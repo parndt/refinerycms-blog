d9bd0d1f012a714063b80bdb27c262757c090733
Philip Arndt <parndt@gmail.com>
2010-07-24 11:57:46 +1200

    Hook into i18n on the backend form and make Joe's replace function work with multiple matches for the permalink.

--- rails/init.rb
+++ rails/init.rb
@@ -1,5 +1,6 @@
 Refinery::Plugin.register do |plugin|
   plugin.title = "Blog"
+  plugin.name = "blog"
   plugin.description = "Manage Blog"
   plugin.version = 1.0
   plugin.menu_match = /admin\/(blog|comments|blog_comments)/
